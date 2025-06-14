70c753ce3203dea67ae1b246e274d9bfe0a0f953
acrdiaz <acrdiaz@msn.com>
2025-06-14 00:33:55 -0400

    add hi.go

---
+++ src/main/hi.go
@@ -0,0 +1,16 @@
+// cmd set variables
+// set GOPATH=<root_dir>\github\Go\
+// set GOBIN=<root_dir>\github\Go\bin
+
+// run the program
+// go run src/main/hi.go
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hi")
+}
